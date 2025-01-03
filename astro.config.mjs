b4729e8bdcf437ed766bec08829ca65e6fcbb595
Rizaaal <gabrieledigrazia@protonmail.com>
2025-01-03 20:55:34 +0100

    prefix import-meta.env.BASE_PATH in navigation paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rizaaal.github.io',
-  base: 'astro_blog/',
+  base: '/astro_blog',
 });
