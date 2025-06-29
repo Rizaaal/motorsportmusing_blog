5f2e141db8b6540c27c08bed622663172cb1fa14
Rizal <gabrieledigrazia@protonmail.com>
2025-06-29 12:49:39 +0200

    fix: last fixes vefore 1.0

--- src/utils/scripts/utils.js
+++ src/utils/scripts/utils.js
@@ -1,10 +1,10 @@
 import dateUtils from "./date";
 
-export function getPostURL(post){
+export function getPostURL(post) {
   const postDate = new Date(post.frontmatter.pubDate);
   const year = postDate.getFullYear();
   const month = dateUtils.monthNames[postDate.getMonth()];
   const title = post.frontmatter.title;
 
-  return `${import.meta.env.BASE_URL}/posts/${year}/${month}/${title}`
+  return `${import.meta.env.BASE_URL}posts/${year}/${month}/${title}`;
 }
