bdff2224962fead66b88541ee2aa50571407e265
Rizal <gabrieledigrazia@protonmail.com>
2025-06-29 11:41:16 +0200

    fix: search with pagefind UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rizaaal.github.io',
-  base: '/astro_blog',
+  site: "https://rizaaal.github.io",
+  base: "/astro_blog/",
+  build: {
+    format: "file",
+  },
+  integrations: [pagefind()],
 });
--- src/utils/scripts/dateToHTML.js
+++
@@ -1,5 +0,0 @@
-export default function(pubDate){
-  //TODO: make a function that gets a pubDate and returns HTML with a:
-  //month title
-  //list of post from that date
-}
